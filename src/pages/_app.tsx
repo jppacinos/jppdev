00d85c59bc6aef775535c979cea0516101b5ca9b
jppacinos <jp.pacinos@gmail.com>
2023-04-10 10:50:51 +0000

    change app font to montserrat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import type { AppProps } from 'next/app'
+import { Montserrat } from 'next/font/google'
 import '@/styles/globals.css'
 import AppMain from '@/layouts/AppMain'
 import useTheme from '@/hooks/useTheme'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+  variable: '--font-montserrat',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   useTheme()
 
   return (
-    <AppMain>
-      <Component {...pageProps} />
-    </AppMain>
+    <div className={`${montserrat.variable} font-sans`}>
+      <AppMain>
+        <Component {...pageProps} />
+      </AppMain>
+    </div>
   )
 }
