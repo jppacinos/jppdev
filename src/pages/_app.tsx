afe81b7f796d80cc2fa0701e725c4a6b8ef8918d
jppacinos <jp.pacinos@gmail.com>
2023-04-14 15:05:10 +0000

    google analytics setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Montserrat } from 'next/font/google'
+import Script from 'next/script'
 import '@/styles/globals.css'
 import AppMain from '@/layouts/AppMain'
 import useTheme from '@/hooks/useTheme'
@@ -14,10 +15,26 @@ export default function App({ Component, pageProps }: AppProps) {
   useTheme()
 
   return (
-    <div className={`${montserrat.variable} font-sans`}>
-      <AppMain>
-        <Component {...pageProps} />
-      </AppMain>
-    </div>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
+        `}
+      </Script>
+
+      <div className={`${montserrat.variable} font-sans`}>
+        <AppMain>
+          <Component {...pageProps} />
+        </AppMain>
+      </div>
+    </>
   )
 }
